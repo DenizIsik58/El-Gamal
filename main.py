34b3f551b62dc090a29cba2ede4952efc084a1f4
Deniz Isik <deniz_4600@live.dk>
2022-09-28 12:45:26 +0200

    Finish hand in 1

--- main.py
+++ main.py
@@ -8,23 +8,24 @@ m = 2000
 bob_private_key = 66
 bob_public_key = 2227
 
+
 # 1
+def encrypt_message(message):
+    pk = bob_public_key
+
+    r = random.randint(1, 6661)
+    c1 = (g**r) % p
+    c2 = message * ((pk**r) % p)
+    c = (c1, c2)
+    return c
+
+# 2
 def bruteforce_x():
     for i in range (1, 241):
         pk = 666 ** i % p
         if pk == 2227:
             print("Found x: " + str(i))
             return i
-
-# 1
-def encrypt_message(message):
-    pk = bob_public_key
-
-    r = random.randint(1, 1000)
-    c1 = g**r
-    c2 = message * ((pk**r) % p)
-    c = (c1, c2)
-    return c
 
 # 2
 def decrypt_message():
@@ -33,28 +34,29 @@ def decrypt_message():
     return decrypted
 
 
-#
-def change_message_to(changed):
-    return encrypt_message(changed)
-
-
 def main():
     # assignment 1
-    #bruteforce_x()
-    #encrypt_message(2000)
+    print("Assignment 1")
+    print(encrypt_message(2000))
+    print()
 
     # assignment 2
-    #decrypt_message()
+    print("Assignment 2")
+    bruteforce_x()
+    print(decrypt_message())
+    print()
 
     # assignment 3
-    #Encrypt the message 2000 to bob
+    print("Assignment 3")
+    # Encrypt the message 2000 to bob
     alice_to_bob_encrypted = encrypt_message(2000)
 
-    # Catch Alice's message and change it to 6000 using bobs public key
-    alice_to_bob_encrypted = change_message_to(6000)
-
+    # Catch Alice's message and change it to 6000 by multiplying with 3
     # deconstruct ciphertext c1 & c2
     mallory_c1, mallory_c2 = alice_to_bob_encrypted
+    mallory_c2 = mallory_c2 * 3
+
+    alice_to_bob_encrypted = (mallory_c1, mallory_c2);
 
     # Bob receives the message and decrypts it. He gets 6000
     decrypted = mallory_c2 / (mallory_c1 ** (bob_private_key) % p)
